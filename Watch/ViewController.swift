3a5ea3b3651d6b35c7033a7b2906a196756c14b2
Naveen Kumar <naveenkumar@Naveens-MacBook-Pro.local>
2024-07-09 22:24:08 +0530

    add files

--- Watch/ViewController.swift
+++ Watch/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("commes to moviez branch")
         // Do any additional setup after loading the view.
     }
 
